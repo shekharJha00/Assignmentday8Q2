d2a0d4aaacf49f5ac8a70ad8c3422e105e9b5555
shekhar <shekharjha7417870578@gmail.com>
2022-08-11 10:20:33 +0530

    Added uc2 : Equals method

--- src/com/bridgelabz/linecomparision/LineComparison.java
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.linecomparision;
 
+
 import java.util.Scanner;
 
 public class LineComparison {
@@ -8,20 +9,40 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
 
-        int x1,y1,x2,y2;
-        double res;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter x coordinates of first point ");
-        x1 = scan.nextInt();
-        System.out.println("Enter y coordinates of first point ");
-        y1 = scan.nextInt();
-        System.out.println("Enter x coordinates of Second point ");
-        x2 = scan.nextInt();
-        System.out.println("Enter y coordinates of Second point ");
-        y2 = scan.nextInt();
+        int x1, y1, x2, y2;
 
-        res = Math.sqrt((Math.pow((x2-x1),2)+Math.pow((y2-y1),2)));
-        System.out.println("Length of the line is : " + res);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter x coordinates of first point ");
+        x1 = scanner.nextInt();
+        System.out.println("Enter y coordinates of first point ");
+        y1 = scanner.nextInt();
+        System.out.println("Enter x coordinates of Second point ");
+        x2 = scanner.nextInt();
+        System.out.println("Enter y coordinates of Second point ");
+        y2 = scanner.nextInt();
+
+        System.out.println("Enter x1 of Third Point: ");
+        int p1 = scanner.nextInt();
+        System.out.println("Enter y1 of Third Point: ");
+        int q1 = scanner.nextInt();
+        System.out.println("Enter x2 of Forth Point: ");
+        int p2 = scanner.nextInt();
+        System.out.println("Enter y2 of Forth Point: ");
+        int q2 = scanner.nextInt();
+        scanner.close();
+
+
+        Double lengthOfLine1 = Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+        Double lengthOfLine2 = Math.sqrt(Math.pow(p2 - p1, 2) + Math.pow(q2 - q1, 2));
+        System.out.printf("Length of First Line is : %.2f %n", +lengthOfLine1);
+        System.out.printf("Length of Second Line is : %.2f %n", +lengthOfLine2);
+
+        boolean ans = lengthOfLine1.equals(lengthOfLine2);
+        if (ans) {
+            System.out.println("Both Lines are Equal");
+        } else {
+            System.out.println("Both Lines are not Equal ");
+        }
     }
-}
+    }
 
